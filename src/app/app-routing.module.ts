bb3231c8efc50a48bb78369054800fbfa870ed01
Jeysi <jessaclaire2022@gmail.com>
2026-03-25 21:46:01 +0800

    Updated app-routing.module.ts with new routing definitions

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup', // Added this block
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
+    path: 'technodev',
+    loadChildren: () => import('./TechnoDev/technodev.module').then(m => m.TechnoDevPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home', // Change this to 'technodev' to see it immediately
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
